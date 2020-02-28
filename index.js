c834a60f2fa30aca93ed8b99a9a95e7e42732ebf
Bret Comnes <bcomnes@gmail.com>
2020-02-27 18:50:01 -0700

    chore: add deps and a build step

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const exec = require('child_process').exec
+const core = require('@actions/core')
 const path = require('path')
 
 const scriptPath = path.resolve(__dirname, 'run.sh')
 
-exec(`sh ${scriptPath}`, (error, stdout, stderr) => {
+const version = core.getInput('version')
+
+exec(`${scriptPath} ${version}`, (error, stdout, stderr) => {
   console.log(stdout)
   console.log(stderr)
   if (error !== null) {
